e669b802cc0b174f2c66a753fd42b95183e4bf1b
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-11-10 15:32:34 +0000

    Index

--- RuScrew/alrt.py
+++ RuScrew/alrt.py
@@ -1,25 +1,10 @@
-import os
+from plyer import notification
 
-def displayNotification(message,title=None,subtitle=None,soundname=None):
-	"""
-		Display an OSX notification with message title an subtitle
-		sounds are located in /System/Library/Sounds or ~/Library/Sounds
-	"""
-	titlePart = ''
-	if(not title is None):
-		titlePart = 'with title "{0}"'.format(title)
-	subtitlePart = ''
-	if(not subtitle is None):
-		subtitlePart = 'subtitle "{0}"'.format(subtitle)
-	soundnamePart = ''
-	if(not soundname is None):
-		soundnamePart = 'sound name "{0}"'.format(soundname)
+def display_notification(message, title="Notification"):
+    """
+    Display a notification using plyer.
+    """
+    notification.notify(title=title, message=message)
 
-	appleScriptNotification = 'display notification "{0}" {1} {2} {3}'.format(message,titlePart,subtitlePart,soundnamePart)
-	os.system("osascript -e '{0}'".format(appleScriptNotification))
-
-if __name__ == '__main__':
-	#displayNotification("message","title","subtitle","Pop")
-	#displayNotification("message","title","subtitle")
-	displayNotification("message","title")
-	#displayNotification("message")
+# Example usage:
+display_notification("Hello, World!", "Test")
--- RuScrew/class.py
+++ RuScrew/class.py
@@ -6,6 +6,7 @@ import webbrowser
 import requests
 from bs4 import BeautifulSoup
 import re, lxml, json
+import time
 
 OPEN_INDEX_NUMBERS = "https://sis.rutgers.edu/soc/api/openSections.json?year=2024&term=1&campus=NB"
 
@@ -31,7 +32,7 @@ def start_checking(dindex):
                 print(f"{index} : open section")
                 print(getRegLink(index))
                 ignore_list.append(index)
-                webbrowser.open(url, new=0, autoraise=True)
+                webbrowser.open(getRegLink(index), new=0, autoraise=True)
 
             else:   
                 print(f"{index} : closed section")
@@ -39,4 +40,6 @@ def start_checking(dindex):
         print("~~~~~~~~~~~~~~~~~~")
 
 if __name__ == '__main__':
-    start_checking(['17322', '17323']);
+    time.sleep(3)
+    discrete_indexes = ['06779', '06778', '06777', '06776', '06780']
+    start_checking(discrete_indexes);
